212ed629d6da8a57ea1436dd82efc99a0e4c5345
NicolasMamani <niko_mamani@hotmail.com>
2023-12-18 13:28:50 -0300

    subiendo 6ta clase

--- src/app.js
+++ src/app.js
@@ -1,86 +1,60 @@
-const express = require("express");
-const app = express();
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   res.end('Mi primer servidor');
+// });
+
 const PUERTO = 8080;
 
-const clientes = [
-  {
-    id: "1",
-    nombre: "Lionel",
-    apellido: "Messi",
-  },
-  {
-    id: "2",
-    nombre: "Fideo",
-    apellido: "Di maría",
-  },
-  {
-    id: "3",
-    nombre: "Dibu",
-    apellido: "Martinez",
-  },
+// server.listen(PUERTO, () => {
+//   console.log('Estamos en escucha');
+// });
+
+const express = require('express');
+// creamos una app
+const app = express();
+
+// creamos una ruta
+app.get('/', (req, res) => {
+  res.send('Mi primer servidor con Express');
+});
+
+app.listen(PUERTO, () => {
+  console.log('Escuchando en el LocalHost');
+});
+
+// creamos otra ruta
+app.get('/tienda', (req, res) => {
+  res.send('Bienvenido a nuestra tienda');
+});
+
+app.get('/contacto', (req, res) => {
+  res.send('Bienvenidos a contacto');
+});
+
+const misProductos = [
+  { id: 1, nombre: 'fideos', precio: 150 },
+  { id: 2, nombre: 'arroz', precio: 200 },
+  { id: 3, nombre: 'aceite', precio: 900 },
 ];
 
-// ruta raiz
-app.get("/", (req, res) => {
-  res.send(clientes);
+app.get('/productos/:id', (req, res) => {
+  //res.send(misProductos);
+  let id = req.params.id;
+  const producto = misProductos.find((item) => item.id == id);
+  producto ? res.send(producto) : res.send('Producto no encontrado');
 });
 
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// ruta get ":id"
-app.get("/:id", (req, res) => {
-  let { id } = req.params;
-  const clienteEncontrado = clientes.find((cliente) => cliente.id == id);
-  if (clienteEncontrado) {
-    return res.send(clienteEncontrado);
-  } else {
-    return res.send("Error al encontrar al cliente");
-  }
+app.get('/clientes', (req, res) => {
+  let { nombre, apellido } = req.query;
+  res.send(`Bienvenido ${nombre} ${apellido}`);
 });
 
-// POST
-app.post("/", (req, res) => {
-  const clienteNuevo = req.body;
-  clientes.push(clienteNuevo);
-  console.log(clientes);
-  res.send({ status: "success", mesagge: "cliente creado" });
-});
-
-// PUT
-app.put("/:id", (req, res) => {
-  // siempre guardar el parametro de id;
-  const { id } = req.params;
-  const { nombre } = req.body;
-  const { apellido } = req.body;
-
-  const clienteEncontrado = clientes.findIndex((cliente) => cliente.id == id);
-  if (clienteEncontrado !== -1) {
-    clientes[clienteEncontrado].nombre = nombre;
-    clientes[clienteEncontrado].apellido = apellido;
-    console.log(clientes);
-    res.send({ status: "success", message: "recurso  encontrado" });
-  } else {
-    res
-      .status(404)
-      .send({ status: "error", mesagge: "cliente no actualizado" });
-  }
-});
-
-// DELETE
-app.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  const clienteIndex = clientes.findIndex((cliente) => cliente.id === id);
-  if (clienteIndex !== -1) {
-    clientes.splice(clienteIndex, 1);
-    console.log(clientes);
-    res.send({ status: "success", mesagge: "Cliente eliminado" });
-  } else {
-    res.status(404).send({ status: "error", mesagge: "cliente no encontrado" });
-  }
-});
-
-// Por lo general, el listen lo dejamos abajo de todo.
-// Siempre debe estar
-app.listen(PUERTO, () => {
-  console.log(`Escuchando en http://localhost:${PUERTO}`);
+// para el límite voy a trabajar con query
+app.get('/product', (req, res) => {
+  let limit = parseInt(req.query.limit);
+  const productos = misProductos.slice(0, limit);
+  res.send(productos);
 });
